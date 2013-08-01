3b67b4c2ca40f3d3aea998b9e57b29f3d3a60654
Martin Høst Normark <m@martinnormark.com>
2013-08-01 23:24:41 +0200

    Add HomeController with Index view

---
+++ EFPlays.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using System.Web.Mvc;
+
+namespace EFPlays.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		//
+		// GET: /Home/
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
